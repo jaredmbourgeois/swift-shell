84624b335e90869e3fcd0f3d0ce54a979702055a
Jared Bourgeois <mail@jaredbourgeois.com>
2022-08-06 03:49:23 -0500

    004 name

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Shell",
-            targets: ["swift-shell"]
+            targets: ["Shell"]
         ),
     ],
     dependencies: [
@@ -28,8 +28,8 @@ let package = Package(
             dependencies: []
         ),
         .testTarget(
-            name: "swift-shell-tests",
-            dependencies: ["swift-shell"]
+            name: "ShellTests",
+            dependencies: ["Shell"]
         ),
     ]
 )
