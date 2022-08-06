9376c45b0a99bef9364e74a4c08ee03bce0d6421
Jared Bourgeois <mail@jaredbourgeois.com>
2022-08-06 03:22:18 -0500

    001 swift-shell

---
+++ Sources/swift-shell/swift_shell.swift
@@ -0,0 +1,63 @@
+import Foundation
+
+public enum Shell {
+  public static let defaultShellPath = "/bin/bash"
+
+  public enum Result {
+    case output(String)
+    case error(String)
+    case failure
+  }
+}
+
+extension Shell {
+  public actor Executor {
+    public nonisolated let shellPath: String
+
+    public init(
+      shellPath: String = Shell.defaultShellPath
+    ) {
+      self.shellPath = shellPath
+    }
+  }
+}
+
+extension Shell.Executor {
+  public func `do`(_ command: String) async -> Shell.Result {
+    await shellProcess(command: command)
+  }
+
+  public func sudo(_ command: String, password: String) async -> Shell.Result {
+    await shellProcess(command: command, password: password)
+  }
+
+  private func shellProcess(
+    command: String,
+    password: String? = nil
+  ) async -> Shell.Result {
+    let process = Process()
+    process.launchPath = shellPath
+    let outputPipe = Pipe()
+    let errorPipe = Pipe()
+    process.standardOutput = outputPipe
+    process.standardError = errorPipe
+    process.standardInput = password
+    process.arguments = ["-c", command]
+    process.launch()
+    process.waitUntilExit()
+    guard process.terminationStatus == 0 else { return .failure }
+    if let output = String(
+      data: outputPipe.fileHandleForReading.readDataToEndOfFile(),
+      encoding: .utf8
+    ) {
+      return .output(output)
+    }
+    if let error = String(
+      data: errorPipe.fileHandleForReading.readDataToEndOfFile(),
+      encoding: .utf8
+    ) {
+      return .error(error)
+    }
+    return .failure
+  }
+}
